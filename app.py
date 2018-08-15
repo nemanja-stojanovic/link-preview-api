f1eaf1e049036a77e4b240c76d8f1b03c9ee5382
Nemanja <s.nemanja@protonmail.ch>
2018-08-15 20:39:04 +0200

    Added package for handling CORS

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from flask import jsonify
 import requests
 import bs4
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/<path:url>", methods=['GET'])
 def link_preview(url):
